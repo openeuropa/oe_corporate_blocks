48195eadc28ed8bd672bb7d8f2265dbd8c15db86
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-03-11 17:10:22 +0100

    OPENEUROPA-1695: Add parameters in service container for footer block.

--- src/Plugin/Block/FooterBlock.php
+++ src/Plugin/Block/FooterBlock.php
@@ -6,8 +6,9 @@ namespace Drupal\oe_corporate_blocks\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides an OpenEuropa Footer block.
@@ -18,7 +19,44 @@ use Symfony\Component\Yaml\Yaml;
  *   category = @Translation("OE Corporate blocks"),
  * )
  */
-class FooterBlock extends BlockBase {
+class FooterBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+
+  /**
+   * Data for footer block from service container.
+   *
+   * @var array
+   */
+  protected $footerData;
+
+  /**
+   * Constructs an Search block object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $block_footer
+   *   The footer block data from parameters of service container.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $block_footer) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->footerData = $block_footer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('block.footer')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -32,8 +70,7 @@ class FooterBlock extends BlockBase {
    */
   public function build() {
     $build['#theme'] = 'corporate_blocks_footer';
-    $footer_data = Yaml::parseFile(drupal_get_path('module', 'oe_corporate_blocks') . '/config/blocks/footer.yml');
-    $build['#corporate_footer'] = $footer_data['corporate_footer'];
+    $build['#corporate_footer'] = $this->footerData['corporate_footer'];
     return $build;
   }
 
