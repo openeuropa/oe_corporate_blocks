564d454c4cdaf0a4398267df1c04708a49f1ca9e
Voidtek <vdacosta@voidtek.com>
2019-08-26 12:58:35 +0100

    OPENEUROPA-2159: Fix typo post_update.

--- oe_corporate_blocks.post_update.php
+++ oe_corporate_blocks.post_update.php
@@ -10,7 +10,7 @@ declare(strict_types = 1);
 /**
  * Delete the config for Site Switcher.
  */
-function oe_theme_helper_post_update_00001_delete_config_of_site_switcher() {
+function oe_corporate_blocks_post_update_00001_delete_config_of_site_switcher() {
   \Drupal::configFactory()->getEditable('oe_corporate_blocks.data.site_switcher')
     ->delete();
 }
