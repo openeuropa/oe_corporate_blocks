78a312678e34522f26c342fa1e2ab232e14d9900
Voidtek <vdacosta@voidtek.com>
2019-08-26 12:32:29 +0100

    OPENEUROPA-2159: Change number of the post_update function.

--- oe_corporate_blocks.post_update.php
+++ oe_corporate_blocks.post_update.php
@@ -8,9 +8,9 @@
 declare(strict_types = 1);
 
 /**
- * Delete the block siteswitcherblock.
+ * Delete the config for Site Switcher.
  */
-function oe_theme_helper_post_update_8201_delete_config_of_site_switcher() {
+function oe_theme_helper_post_update_00001_delete_config_of_site_switcher() {
   \Drupal::configFactory()->getEditable('oe_corporate_blocks.data.site_switcher')
     ->delete();
 }
