d96ba95743cdc42ebb36ab6533c070a5f142736c
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-10-11 17:49:13 +0200

    OPENEUROPA-2394: Add upgrade path.

---
+++ oe_corporate_blocks.post_update.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * OpenEuropa Corporate Blocks post updates.
+ */
+
+declare(strict_types = 1);
+
+/**
+ * Update corporate footer.
+ */
+function oe_corporate_blocks_post_update_00001(&$sandbox) {
+  $config = \Drupal::configFactory()->getEditable('oe_corporate_blocks.data.footer');
+  $config->set('about_ec_links', [
+    [
+      'href' => 'https://ec.europa.eu/info/index_en',
+      'label' => 'European Commission website',
+    ]
+  ]);
+  $config->save();
+}
