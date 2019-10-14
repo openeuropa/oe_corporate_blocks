843f2ab33d18aab275cbc5884a3ae4769399c1f4
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-10-14 16:54:05 +0200

    OPENEUROPA-2394: Fix of config translation issue.

--- oe_corporate_blocks.post_update.php
+++ oe_corporate_blocks.post_update.php
@@ -19,23 +19,12 @@ function oe_corporate_blocks_post_update_10001(&$sandbox): void {
     ],
   ]);
   $config->save();
-  // We have to update configuration also for translated configs manually.
-  $language_manager = \Drupal::languageManager();
-  $default_language = $language_manager->getDefaultLanguage()->getId();
-  foreach ($language_manager->getLanguages() as $language) {
-    if ($default_language === $language->getId()) {
-      continue;
-    }
-    $config_translation = $language_manager->getLanguageConfigOverride($language->getId(), 'oe_corporate_blocks.data.footer');
-    $config_translation->set(
-      'about_ec_links', [
-        [
-          'href' => 'https://ec.europa.eu/info/index_en',
-          'label' => 'European Commission website',
-        ],
-      ]
-    );
-    $config_translation->save();
-  }
 
+  // Workaround for forcing config translation.
+  /** @var \Drupal\locale\StringDatabaseStorage $storage */
+  $storage = \Drupal::service('locale.storage');
+  $string = $storage->findString(['source' => 'https://ec.europa.eu/info/index_en']);
+  $storage->delete($string);
+  $string = $storage->findString(['source' => 'European Commission website']);
+  $storage->delete($string);
 }
