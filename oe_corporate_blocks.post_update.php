69036003f901049f76eb2547f3864a213dbdf28f
Sergii Pavlenko <sergii.pavlenko.v@gmail.com>
2019-10-16 13:41:11 +0200

    OPENEUROPA-2394: Minor fixes.

--- oe_corporate_blocks.post_update.php
+++ oe_corporate_blocks.post_update.php
@@ -8,7 +8,7 @@
 declare(strict_types = 1);
 
 /**
- * Update corporate footer.
+ * Only display "European Commission website" link in footer.
  */
 function oe_corporate_blocks_post_update_10001(&$sandbox): void {
   $config = \Drupal::configFactory()->getEditable('oe_corporate_blocks.data.footer');
@@ -20,7 +20,9 @@ function oe_corporate_blocks_post_update_10001(&$sandbox): void {
   ]);
   $config->save();
 
-  // Workaround for forcing config translation.
+  // Allow for config translation re-import when running
+  // "drush oe-multilingual:import-local-translations".
+  // @see https://webgate.ec.europa.eu/CITnet/jira/browse/OPENEUROPA-2407
   /** @var \Drupal\locale\StringDatabaseStorage $storage */
   $storage = \Drupal::service('locale.storage');
   $string = $storage->findString(['source' => 'https://ec.europa.eu/info/index_en']);
